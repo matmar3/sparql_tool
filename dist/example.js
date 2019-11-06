a9835c5dedda08c8416205da253e8fb288b4b051
matmar3 <nouzovekonto@gmail.com>
2019-11-06 11:35:37 +0100

    Finalization

--- dist/example.js
+++ dist/example.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
+    // Copy value into textarea
     $('.query-example').click(function() {
         var query = $(this).data("query");
     
         $("#query-input").val(query);
     });
     
-    $('#submit-query').click(function(event) {
+    // send query to Viruoso or Fuseki endpoint
+    $('.submit-query').click(function(event) {
         event.preventDefault();
     
         var query = $('#query-input').val();
         var sparql = new SPARQL("#example-sparql-results");
-        var virtuoso_url = "http://dbpedia.org/sparql";
-        var fuseki_url = "http://localhost:3030/DBM2/query";
+        var url = $(this).data("url");
         
-        sparql.plaintext_query(virtuoso_url, query);
-        sparql.plaintext_query(fuseki_url, query);
+        sparql.plaintext_query(url, query);
     });
 });
